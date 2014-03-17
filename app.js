bed02afa85b9ec817d1d8d026365437f06da10e4
bbogar <blaine.bogar@gmail.com>
2014-03-16 22:59:39 -0600

    JS updated

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-//$(document).ready(function(){
-
-
-//var num = 1;
 function fizzBuzz(){
 	for (var num = 1; num <= 100; num++){
 		if(num % 3 === 0 && num % 5 === 0){
-			console.log("fizz buzz");
+			$("body").append(" fizz buzz ");
 		} else if(num % 3 === 0){
-			console.log("fizz");
+			$("body").append(" fizz ");
 		} else if (num % 5 === 0){
-			console.log("buzz");
+			$("body").append(" buzz ");
 		} else {
-			console.log(num);
+			$("body").append(num + " ");
 		}
 	}
 }
 
-//})
+$(document).ready(function(){
+	$("body").append(fizzBuzz);
+})
